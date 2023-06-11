69c6196641b2321ff7ed4dfd767def6ccdff3036
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-06-11 15:41:35 +0700

    add cors

--- api_model.py
+++ api_model.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import api_function as model
 from pydantic import BaseModel
 
@@ -7,6 +8,21 @@ class textBody(BaseModel):
     text : str
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
